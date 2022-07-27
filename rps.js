8cc263c73c5bc18a3786a7497da7f7060dc7b9e5
Lawrence Lin <pkmnbumba@gmail.com>
2022-07-27 16:19:03 -0700

    Add game function to handle gameplay

--- rps.js
+++ rps.js
@@ -11,16 +11,37 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
-    return "Tie Game!";
+    console.log("Tie Game!");
+    return 0;
   }
   else if(
     (playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Rock")) {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return -1;
   }
   else {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
   }
 }
 
+function game() {
+  let score = 0;
+  for(let i = 0; i < 5; i++) {
+    let playerSelection = camelCase(prompt("Pick Rock, Paper, or Scissors."));
+    let computerSelection = getComputerChoice();
+    console.log(`You picked ${playerSelection}`);
+    console.log(`Computer picked ${computerSelection}`);
+    score += playRound(playerSelection, computerSelection);
+    console.log(`Current score is ${score}. Round ${i + 1} out of 5.`);
+  }
+}
+
+
+function camelCase(string) {
+  return string.toLowerCase().replace(/^./,string.charAt(0).toUpperCase());
+}
+
+game();
