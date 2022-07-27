000d6bdc186cafec29741a9e29a9f9028e6d92b0
Lawrence Lin <pkmnbumba@gmail.com>
2022-07-27 15:55:28 -0700

    Add getComputerChoice function to randomly select rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -1,1 +1,13 @@
-console.log("Test");
+function getComputerChoice () {
+  switch(Math.floor(Math.random()*3)) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+  }
+}
