293877feb2630f6ca51cf506c064956ce6af7692
Lawrence Lin <pkmnbumba@gmail.com>
2022-07-27 16:09:48 -0700

    Add playRound function to compare selections and decide winner

--- rps.js
+++ rps.js
@@ -1,13 +1,26 @@
-function getComputerChoice () {
-  switch(Math.floor(Math.random()*3)) {
-    case 0:
-      return "Rock";
-      break;
-    case 1:
-      return "Paper";
-      break;
-    case 2:
-      return "Scissors";
-      break;
+function getComputerChoice() {
+  switch (Math.floor(Math.random() * 3)) {
+  case 0:
+    return "Rock";
+  case 1:
+    return "Paper";
+  case 2:
+    return "Scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === playerSelection) {
+    return "Tie Game!";
+  }
+  else if(
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  }
+  else {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+  }
+}
+
